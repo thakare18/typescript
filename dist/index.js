73a0e5ca477136229156c39795cd6692cc8f5522
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-28 00:07:55 +0530

    typescript program lec-4

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// function in typeScript
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello, ${person} welcome to ChaiAurcode!`;
+}
+// variable in typeScript
+const username = "Chai aur typeScript";
+console.log(greet(username)); // calling the function
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// function in typeScript
+
+function greet(person : string): string {
+    return `Hello, ${person} welcome to ChaiAurcode!`;
+}
+
+// variable in typeScript
+ const username : string = "Chai aur typeScript";
+ console.log(greet(username)); // calling the function
