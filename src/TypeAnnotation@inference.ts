1ac3a6a7ad1dedbabbf5660ac4ad8549211aaf71
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-28 00:46:01 +0530

    Type Annotation and Type Inference in TypeScript

---
+++ src/TypeAnnotation@inference.ts
@@ -0,0 +1,13 @@
+// Type Annotation:
+// When we manually specify the type of a variable or function.
+// Syntax: variableName: type
+
+let age: number = 21;
+let name: string = "Prathamesh";
+
+
+// Type Inference:
+// TypeScript automatically guesses the type based on the value assigned.
+
+let city = "Mumbai"; // TypeScript infers type as string
+let marks = 90;      // TypeScript infers type as number
