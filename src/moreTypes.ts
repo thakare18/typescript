5ad548a13ea912df7496a145ed86819524188428
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-30 11:20:04 +0530

    typeScript try catch error handling

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -1,3 +1,4 @@
+//Type Assertion and Unknown Type
 let newValue : unknown;
 
 newValue = 10; // number
@@ -8,3 +9,21 @@ newValue = true; // boolean
 if ( typeof newValue === "string") {
     newValue.toUpperCase(); // No error
 }
+
+//try catch block
+
+try {
+
+}
+catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message); // No error
+    }
+    console.log(error); // No error
+}
+
+
+const data: unknown = "New data value";
+const strData : string = data as string; // Type assertion to string
+
+// Never in TypeScript
